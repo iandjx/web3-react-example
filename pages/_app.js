aa819f9f3552195b66afed787a667a8f80c2f0a6
Ian de Jesus <ian@eskwelabs.com>
2022-02-14 19:37:30 +0900

    upgraded packages

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
-import { Web3ReactProvider } from '@web3-react/core'
-import Web3 from 'web3'
-
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
+import * as ethers from "ethers";
+// function getLibrary(provider) {
+//   return new Web3(provider)
+// }
 function getLibrary(provider) {
-  return new Web3(provider)
+  return new ethers.providers.Web3Provider(provider);
 }
 
 function MyApp({ Component, pageProps }) {
@@ -12,7 +15,7 @@ function MyApp({ Component, pageProps }) {
     <Web3ReactProvider getLibrary={getLibrary}>
       <Component {...pageProps} />
     </Web3ReactProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,69 @@
-import { useWeb3React } from "@web3-react/core"
-import { useEffect } from "react"
-import { injected } from "../components/wallet/connectors"
+import { useWeb3React } from "@web3-react/core";
+import { useEffect } from "react";
+import { injected } from "../components/wallet/connectors";
 
 export default function Home() {
-  const { active, account, library, connector, activate, deactivate } = useWeb3React()
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
 
   async function connect() {
+    console.log("connecting");
     try {
-      await activate(injected)
-      localStorage.setItem('isWalletConnected', true)
+      await activate(injected);
+      localStorage.setItem("isWalletConnected", true);
     } catch (ex) {
-      console.log(ex)
+      console.log(ex);
     }
   }
 
   async function disconnect() {
     try {
-      deactivate()
-      localStorage.setItem('isWalletConnected', false)
+      deactivate();
+      localStorage.setItem("isWalletConnected", false);
     } catch (ex) {
-      console.log(ex)
+      console.log(ex);
     }
   }
 
   useEffect(() => {
     const connectWalletOnPageLoad = async () => {
-      if (localStorage?.getItem('isWalletConnected') === 'true') {
+      if (localStorage?.getItem("isWalletConnected") === "true") {
         try {
-          await activate(injected)
-          localStorage.setItem('isWalletConnected', true)
+          await activate(injected);
+          localStorage.setItem("isWalletConnected", true);
+          console.log("connected");
         } catch (ex) {
-          console.log(ex)
+          console.log(ex);
         }
       }
-    }
-    connectWalletOnPageLoad()
-  }, [])
+    };
+    connectWalletOnPageLoad();
+  }, [activate]);
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect to MetaMask</button>
-      {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
-      <button onClick={disconnect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect</button>
+      {console.log(active)}
+      {console.log(connector)}
+      {console.log(library)}
+      <button
+        onClick={connect}
+        className="w-56 py-2 mt-20 mb-4 text-lg font-bold text-white bg-blue-600 rounded-lg hover:bg-blue-800"
+      >
+        Connect to MetaMask
+      </button>
+      {active ? (
+        <span>
+          Connected with <b>{account}</b>
+        </span>
+      ) : (
+        <span>Not connected</span>
+      )}
+      <button
+        onClick={disconnect}
+        className="w-56 py-2 mt-20 mb-4 text-lg font-bold text-white bg-blue-600 rounded-lg hover:bg-blue-800"
+      >
+        Disconnect
+      </button>
     </div>
-  )
+  );
 }
